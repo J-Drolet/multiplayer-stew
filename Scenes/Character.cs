46b7547a02788daf5ad5b484ee37430f430283f5
maxcolt <maxcoltpm@gmail.com>
2025-01-18 20:32:23 -0500

    Moveable character

---
+++ Scenes/Character.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System;
+
+public partial class Character : CharacterBody3D
+{
+	[Export]
+	public Camera3D Camera;
+	[Export]
+	public Node3D Head;
+
+	[Export]
+	public int MouseSensitivity = 50;
+
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+
+
+    public override void _Ready()
+    {
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+		if(Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			if (@event.IsActionPressed("ui_cancel"))
+			{
+                Input.MouseMode = Input.MouseModeEnum.Visible;
+            }
+
+			if(@event is InputEventMouseMotion)
+			{
+				InputEventMouseMotion iEvent = @event as InputEventMouseMotion;
+				Head.RotateY(-iEvent.Relative.X * MouseSensitivity * 0.0001f);
+                Camera.RotateX(-iEvent.Relative.Y * MouseSensitivity * 0.0001f);
+				Camera.RotationDegrees = new Vector3(Math.Clamp(Camera.RotationDegrees.X, -85, 85), 0, 0);
+            }
+		}
+			
+    }
+
+    public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Back");
+		Vector3 direction = (Head.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
