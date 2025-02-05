d06748340ec2ec126e585bd4c2e4be186c9d4e5f
maxcolt <maxcoltpm@gmail.com>
2025-02-04 22:42:14 -0500

    #27 Rifle initial start and selfdestroyingparticle

---
+++ Scripts/Base/SelfDestroyingParticle.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+using multiplayerstew.Scripts.Attributes;
+using System;
+
+
+namespace multiplayerstew.Scripts.Base
+{
+    public partial class SelfDestroyingParticle : Node3D
+    {
+        [Export, ExportRequired]
+        public PackedScene particleScene {  get; set; }
+
+        public void emitParticles()
+        {
+            GpuParticles3D particle3DInstance = particleScene.Instantiate() as GpuParticles3D;
+            AddChild(particle3DInstance);
+            particle3DInstance.Ready += () => particle3DInstance.Emitting = true;
+            particle3DInstance.Finished += particle3DInstance.QueueFree; 
+        }
+    }
+}
