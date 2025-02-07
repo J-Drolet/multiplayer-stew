d73d808e744e6247e40144b83f0f6c3ca215d669
Jack Drolet <jjtelord@gmail.com>
2025-02-06 21:51:06 -0500

    Fix missing script connections

--- Scripts/Pickups/CharacterUpgradePickup.cs
+++ Scripts/Pickups/CharacterUpgradePickup.cs
@@ -14,6 +14,6 @@ public partial class CharacterUpgradePickup : Pickup
         if(!character.Upgrades.Contains(Upgrade))
         {
             character.Rpc(Character.MethodName.AddUpgrade, Variant.From(Upgrade));
-        }
+        } 
     }
 }
