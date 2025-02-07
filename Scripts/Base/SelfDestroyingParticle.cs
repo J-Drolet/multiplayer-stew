1cfbac3505f6b9572881e52832a8f093354026f1
maxcolt <maxcoltpm@gmail.com>
2025-02-07 15:14:51 -0500

    #25 Pistol particles

--- Scripts/Base/SelfDestroyingParticle.cs
+++ Scripts/Base/SelfDestroyingParticle.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using multiplayerstew.Scripts.Attributes;
+using multiplayerstew.Scripts.Services;
 using System;
 
 
@@ -10,10 +11,16 @@ namespace multiplayerstew.Scripts.Base
         [Export, ExportRequired]
         public PackedScene particleScene {  get; set; }
 
+        public override void _Ready()
+        {
+            GodotErrorService.ValidateRequiredData(this);
+        }
+
         public void emitParticles()
         {
             GpuParticles3D particle3DInstance = particleScene.Instantiate() as GpuParticles3D;
             AddChild(particle3DInstance);
+            particle3DInstance.GlobalRotation = GlobalRotation;
             particle3DInstance.Ready += () => particle3DInstance.Emitting = true;
             particle3DInstance.Finished += particle3DInstance.QueueFree; 
         }
