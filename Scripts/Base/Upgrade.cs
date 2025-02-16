2c6ab85432f81148f725550cca07df7ddd06a21f
Jack Drolet <jjtelord@gmail.com>
2025-02-16 13:33:19 -0500

    Add Upgrade Pickups

--- Scripts/Base/Upgrade.cs
+++ Scripts/Base/Upgrade.cs
@@ -8,11 +8,9 @@ public enum Upgrade {
     C_FastSlide,
     C_SmallerHitbox,
     C_OutlinePlayers,
-    W_IncreasedClipSlowerReload,
     W_DunceCap,
     W_SwitchFiremode,
     W_BouncyProjectile,
-    W_ShotgunFiring,
     W_MagDump,
     W_Homing,
     W_SlowTargetBullets
