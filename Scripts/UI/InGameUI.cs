6d9ef7091d9a4dea2dd2364a803cb4bf90b4ed0b
Jack Drolet <jjtelord@gmail.com>
2025-02-22 19:44:34 -0500

    #62 Cigarette health bar first pass

--- Scripts/UI/InGameUI.cs
+++ Scripts/UI/InGameUI.cs
@@ -1,33 +1,22 @@
 using Godot;
 using multiplayerstew.Scripts.Attributes;
 using multiplayerstew.Scripts.Services;
-using System;
 
 public partial class InGameUI : Control
 {
     [Export, ExportRequired]
     public AmmoDisplay AmmoCount { get; set; }
     [Export, ExportRequired]
-    public TextureRect HealthBar { get; set; } = new TextureRect();
+    public Range HealthBar { get; set; }
 
     public override void _Ready()
     {
         GodotErrorService.ValidateRequiredData(this);
         UI.InGameUI = this;
-        HealthBar.Texture = new GradientTexture1D();
-        (HealthBar.Texture as GradientTexture1D).Gradient = new();
     }
 
     public void SetHealthBar(float healthPercent)
     {
-        float healthOffset = healthPercent;
-
-        
-        //RED POINT
-        (HealthBar.Texture as GradientTexture1D).Gradient.SetOffset(0, healthOffset);
-        (HealthBar.Texture as GradientTexture1D).Gradient.SetColor(0, new Color(0.85f, 0.00f, 0.00f, 1.0f));
-        //CLEAR POINT
-        (HealthBar.Texture as GradientTexture1D).Gradient.SetOffset(1, healthOffset + 0.0001f);
-        (HealthBar.Texture as GradientTexture1D).Gradient.SetColor(1, new Color(1.0f, 1.0f, 1.0f, 0.0f));
+        HealthBar.Value = healthPercent * 100;
     }
 }
