af443d3db8b457b25de701383eb74b13e13a37dc
Jack Drolet <jjtelord@gmail.com>
2025-03-11 17:39:39 -0400

    Add power level indicator on pickup

--- Scripts/UI/InGameUI/FlavorTextDisplay.cs
+++ Scripts/UI/InGameUI/FlavorTextDisplay.cs
@@ -8,6 +8,8 @@ public partial class FlavorTextDisplay : Control
     public Label TitleLabel;
     [Export, ExportRequired]
     public Label SubTitleLabel;
+    [Export, ExportRequired]
+    public Label PowerLevelLabel;
     [Export]
     public double TotalDisplayTime;
     [Export, ExportRequired]
@@ -35,26 +37,50 @@ public partial class FlavorTextDisplay : Control
         }
     }
 
-    private void DisplayFlavorText(string title, string subtitle)
+    private void DisplayFlavorText(string title, string subtitle, int powerLevelChange)
     {
         DisplayTime = 0;
         TitleLabel.Text = title;
         SubTitleLabel.Text = subtitle;
         Modulate = Colors.White;
+
+        if(powerLevelChange > 0)
+        {
+            PowerLevelLabel.Modulate = Colors.Green;
+            PowerLevelLabel.Text = $"+{powerLevelChange}";
+        }
+        else if(powerLevelChange < 0)
+        {
+            PowerLevelLabel.Modulate = Colors.Red;
+            PowerLevelLabel.Text = $"{powerLevelChange}";
+        }
+        else
+        {
+            PowerLevelLabel.Modulate = Colors.White;
+            PowerLevelLabel.Text = $"+{powerLevelChange}";
+        }
+
     }
 
-    public void DisplayFlavorTextFor(Weapon weapon)
+    public void DisplayFlavorTextFor(Weapon weapon, Weapon? oldWeapon = null)
     {
         string title = (string)Config.GetValue("Weapon." + weapon.ToString(), "flavor_title", true);
         string subtitle = (string)Config.GetValue("Weapon." + weapon.ToString(), "flavor_subtitle", true);
-        DisplayFlavorText(title, subtitle);
+
+        int weaponPowerLevel = PowerLevelService.GetPowerLevel(weapon);
+        if(oldWeapon.HasValue)
+        {
+            weaponPowerLevel -= PowerLevelService.GetPowerLevel(oldWeapon.Value);
+        }
+
+        DisplayFlavorText(title, subtitle, weaponPowerLevel);
     }
 
     public void DisplayFlavorTextFor(Upgrade upgrade)
     {
         string title = (string)Config.GetValue("Upgrade." + upgrade.ToString(), "flavor_title", true);
         string subtitle = (string)Config.GetValue("Upgrade." + upgrade.ToString(), "flavor_subtitle", true);
-        DisplayFlavorText(title, subtitle);
+        DisplayFlavorText(title, subtitle, PowerLevelService.GetPowerLevel(upgrade));
     }
 
     public void HideDisplay() 
