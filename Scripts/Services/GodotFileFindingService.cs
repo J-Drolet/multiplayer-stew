754eb9a99a3a02cd1db39a4d63b9fc13304362b5
Jack Drolet <jjtelord@gmail.com>
2025-03-12 00:00:58 -0400

    Simplyify .import removal process

--- Scripts/Services/GodotFileFindingService.cs
+++ Scripts/Services/GodotFileFindingService.cs
@@ -5,8 +5,6 @@ using System.Linq;
 
 public partial class GodotFileFindingService : Node
 {
-	public static string[] ImportedFileExtensions = ["png"];
-
     public static List<string> GetScenesAtFilepath(string parentFilepath)
 	{
 		return GetFilesAtFilePath(parentFilepath, ["tscn", "scn", "escn"]);
@@ -20,27 +18,12 @@ public partial class GodotFileFindingService : Node
 		foreach(string filename in dir.GetFiles()) 
 		{
 			string filepath = parentFilepath + filename;
-			string removedRemapFile = filepath.Replace (".remap", ""); // export adds unnecessary .remap extension
-			string extension = GetExtension(removedRemapFile);
+			// When exporting the project, all scenes get a .remap and textures get a .import added that we need to remove so we can access the underlying file
+			string strippedFilepath = filepath.Replace(".remap", "").Replace (".import", ""); 
+			string extension = GetExtension(strippedFilepath);
 			if(extensions.Contains(extension))
 			{
-				files.Add(removedRemapFile);
-			}
-			else // check if no import is present
-			{
-				foreach(string targetedFileExtension in extensions)
-				{
-					if(ImportedFileExtensions.Contains(targetedFileExtension)) // if this is a filetype that is accessed by an import
-					{
-						string removedImportFile = filepath.Replace (".import", "");
-						extension = GetExtension(removedImportFile);
-						if(targetedFileExtension == extension)
-						{
-							files.Add(removedImportFile);
-						}
-					}
-				}
-				
+				files.Add(strippedFilepath);
 			}
 		}
 		return files.ToList();
