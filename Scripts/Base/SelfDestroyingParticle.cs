64fc1f030ab0abfdc18f1add5999debf64d3eff5
Jack Drolet <jjtelord@gmail.com>
2025-03-25 23:35:43 -0400

    #75 Make muzzle flash show in GunViewCamera

--- Scripts/Base/SelfDestroyingParticle.cs
+++ Scripts/Base/SelfDestroyingParticle.cs
@@ -2,6 +2,7 @@
 using multiplayerstew.Scripts.Attributes;
 using multiplayerstew.Scripts.Services;
 using System;
+using System.Collections.Generic;
 
 
 namespace multiplayerstew.Scripts.Base
@@ -30,23 +31,37 @@ namespace multiplayerstew.Scripts.Base
             {
                 Rpc(MethodName.EmitParticles); // tell other clients to spawn particles if not in a RPC
             }
-            else // only spawn in a RPC
+            else
             {
-                GpuParticles3D particle3DInstance = ParticleScene.Instantiate() as GpuParticles3D;
-                switch(ParticleSpawnMode)
-                {
-                    case SpawnMode.Global:
-                        LevelManager.Instance.AddChild(particle3DInstance);
-                        break;
-                    case SpawnMode.Local:
-                        AddChild(particle3DInstance);
-                        break;
-                }
-                particle3DInstance.GlobalPosition = GlobalPosition;
-                particle3DInstance.GlobalRotation = GlobalRotation;
-                particle3DInstance.Emitting = true;
-                particle3DInstance.Finished += particle3DInstance.QueueFree; 
+                SpawnParticles(1);
             }
+        }
+
+        public void EmitParticlesOnLocalOnlyLayer(int localLayer)
+        {
+            Rpc(MethodName.EmitParticles);
+            SpawnParticles(localLayer);
+        }
+
+        private void SpawnParticles(int visibilityLayer)
+        {
+            GpuParticles3D particle3DInstance = ParticleScene.Instantiate() as GpuParticles3D;
+            particle3DInstance.SetLayerMaskValue(1, false);
+            particle3DInstance.SetLayerMaskValue(visibilityLayer, true);
+
+            switch(ParticleSpawnMode)
+            {
+                case SpawnMode.Global:
+                    LevelManager.Instance.AddChild(particle3DInstance);
+                    break;
+                case SpawnMode.Local:
+                    AddChild(particle3DInstance);
+                    break;
+            }
+            particle3DInstance.GlobalPosition = GlobalPosition;
+            particle3DInstance.GlobalRotation = GlobalRotation;
+            particle3DInstance.Emitting = true;
+            particle3DInstance.Finished += particle3DInstance.QueueFree;
         }
     }
 }
